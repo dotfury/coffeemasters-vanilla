19b0ff5222670c04c6884047849f904e648943ca
dotfury <cm2jr4@gmail.com>
2025-01-14 13:01:42 +0900

    add services - 2025/01/14

---
+++ app.js
@@ -0,0 +1,10 @@
+import Store from './services/Store.js';
+import { loadData } from './services/Menu.js';
+
+window.app = {
+  store: Store
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+  loadData();
+});
