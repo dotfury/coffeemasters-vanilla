a6ba6d362abef4836cf60470c09bb6e8a0976ca4
dotfury <cm2jr4@gmail.com>
2025-01-22 12:55:23 +0900

    Load templates - 2025/01/22

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import Store from './services/Store.js';
 import { loadData } from './services/Menu.js';
 import Router from './services/Router.js';
 
+// Link my web components
+import { MenuPage } from './components/MenuPage.js';
+import { DetailsPage } from './components/DetailsPage.js';
+import { OrderPage } from './components/OrderPage.js';
+
 window.app = {
   store: Store,
   router: Router
