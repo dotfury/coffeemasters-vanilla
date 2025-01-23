9fb73d9b2d3f3e874f352167c86b7b8c17a32854
dotfury <cm2jr4@gmail.com>
2025-01-23 12:55:14 +0900

    load css and create proxy - 2025/01/23

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,19 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: 'open' });
+
+    const styles = document.createElement('style');
+    this.root.appendChild(styles);
+
+    async function loadCSS() {
+      const request = await fetch('/components/MenuPage.css');
+      const css = await request.text();
+      styles.textContent = css;
+    }
+
+    loadCSS();
   }
 
   // when attached to DOM
@@ -8,7 +21,7 @@ export class MenuPage extends HTMLElement {
     const template = document.getElementById('menu-page-template');
     const content = template.content.cloneNode(true);
 
-    this.appendChild(content);
+    this.root.appendChild(content);
   }
 }
 
